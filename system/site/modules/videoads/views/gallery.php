f3c73a18e3e3ad9e993ed3c24071fbfc6a8fe6e5
Ashley <ashley@arbitrarilyarbit.com>
2012-01-13 16:39:58 +0530

    Video gallery demo impl done

--- system/site/modules/videoads/views/gallery.php
+++ system/site/modules/videoads/views/gallery.php
@@ -1,7 +1,16 @@
 <div>YPM TV</div>
-<!-- TODO: Ashley load this with ajax -->
-<img class="ads" src="<?php echo base_url(); ?>system/site/themes/site/img/empty.gif" data-original="<?php echo base_url(); ?>uploads/ads/std/1/1.JPG">
-<img class="ads" src="<?php echo base_url(); ?>system/site/themes/site/img/empty.gif" data-original="<?php echo base_url(); ?>uploads/ads/std/1/2.JPG">
-<img class="ads" src="<?php echo base_url(); ?>system/site/themes/site/img/empty.gif" data-original="<?php echo base_url(); ?>uploads/ads/std/1/3.JPG">
-<img class="ads" src="<?php echo base_url(); ?>system/site/themes/site/img/empty.gif" data-original="<?php echo base_url(); ?>uploads/ads/std/1/4.JPG">
-<img class="ads" src="<?php echo base_url(); ?>system/site/themes/site/img/empty.gif" data-original="<?php echo base_url(); ?>uploads/ads/std/1/1.JPG">
+
+<link rel="stylesheet" href="<?php echo base_url(); ?>system/site/themes/site/css/vid/videolightbox.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>system/site/themes/site/css/vid/overlay-minimal.css"/>
+
+<script src="<?php echo base_url(); ?>system/site/themes/site/js/vid/swfobject.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>system/site/themes/site/js/vid/videolightbox.js" type="text/javascript"></script>
+<!-- TODO: Ashley load this with ajax-->
+<div id="videogallery">
+	<a rel="#voverlay" href="http://www.youtube.com/v/gzS0ZmPsMF4?autoplay=1&rel=0&enablejsapi=1&playerapiid=ytplayer" title="Aishpra 20 Sec B'day"><img src="<?php echo base_url(); ?>uploads/ads/vid/1/thumbs/0.png" alt="Aishpra B'day" /><span>Aishpra Birthday</span></a>
+	<a rel="#voverlay" href="http://www.youtube.com/v/TOK4cM6gNFY?autoplay=1&rel=0&enablejsapi=1&playerapiid=ytplayer" title="Aishpra 20 Sec  dia &amp; gold"><img src="<?php echo base_url(); ?>uploads/ads/vid/1/thumbs/1.png" alt="Aishpra dia &amp; gold" /><span>Aishpra diamond &amp; gold</span></a>
+	<a rel="#voverlay" href="http://www.youtube.com/v/08Yzr39JXpI?autoplay=1&rel=0&enablejsapi=1&playerapiid=ytplayer" title="SMS Group of Institution  15 Sec"><img src="<?php echo base_url(); ?>uploads/ads/vid/1/thumbs/2.png" alt="SMS Group of Institution" /><span>SMS Group of Institution</span></a>
+	<a rel="#voverlay" href="http://www.youtube.com/v/ZL--nS1b0lY?autoplay=1&rel=0&enablejsapi=1&playerapiid=ytplayer" title="Noorani Tel  10Sec"><img src="<?php echo base_url(); ?>uploads/ads/vid/1/thumbs/3.png" alt="Noorani Tel" /><span>Noorani Tel</span></a>
+	<a rel="#voverlay" href="http://www.youtube.com/v/f3Eegkyvi_k?autoplay=1&rel=0&enablejsapi=1&playerapiid=ytplayer" title="HimlokTel  10 sec"><img src="<?php echo base_url(); ?>uploads/ads/vid/1/thumbs/4.png" alt="Himlok Tel" /><span>Himlok Tel</span></a>
+	<a rel="#voverlay" href="http://www.youtube.com/v/y4Hva-83C0I?autoplay=1&rel=0&enablejsapi=1&playerapiid=ytplayer" title="BBS Group of Institutions  15Sec"><img src="<?php echo base_url(); ?>uploads/ads/vid/1/thumbs/5.png" alt="BBS Group of Institutions" /><span>BBS Group of Institutions</span></a>
+</div>
